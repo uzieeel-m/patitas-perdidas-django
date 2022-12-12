df37fe8277bb86acac30f237952489afd72cfa49
Uziel Montes <uzieeel.m@gmail.com>
2022-12-12 15:41:36 -0600

    mascotas p2

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import VistaDetalleMascota, VistaListaMascotas
+from .views import VistaDetalleMascota, VistaListaMascotas, VistaMisMascotas, VistaEditarMascota, VistaEliminarMascota, VistaAgregarMascota
 
 urlpatterns = [
     path('detalle-mascota/<int:pk>', VistaDetalleMascota.as_view(), name="detalle-mascota"),
     path('lista-mascotas', VistaListaMascotas.as_view(), name="lista-mascotas"),
+    path('mis-mascotas', VistaMisMascotas.as_view(), name="mis-mascotas"),
+    path('editar-mascota/<int:pk>', VistaEditarMascota.as_view(), name="editar-mascota"),
+    path('eliminar-mascota/<int:pk>', VistaEliminarMascota.as_view(), name="eliminar-mascota"),
+    path('agregar-mascota/', VistaAgregarMascota.as_view(), name="agregar-mascota"),
 ]
